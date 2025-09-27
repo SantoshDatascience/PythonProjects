418a5dd30044bf124966abbd699e665aa026c7f3
unknown <menonsantosh7@gmail.com>
2025-09-27 22:20:20 +0530

    Add prnt.py

---
+++ prnt.py
@@ -0,0 +1,6 @@
+MAX_AGE = 90
+print("Calculate No of weeks left.")
+current_age = int(input("Enter your current Age:"))
+year_left = MAX_AGE - current_age
+total_weeks = year_left * 52
+print(f"Weeks left=>{total_weeks}.")
